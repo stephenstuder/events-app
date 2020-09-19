257153dddb3af58da322d50576137455ee4bc62a
Stephen Studer <stephenkstuder@gmail.com>
2020-09-18 19:06:22 -0700

    formatting

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
-  withCredentials: false, // This is the default
+  withCredentials: false, // This is the default value
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
