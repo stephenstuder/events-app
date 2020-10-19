c86b6f4ddd5e5db0b6ed35e425a6d981c2e97094
Stephen Studer <stephenkstuder@gmail.com>
2020-10-19 07:55:48 -0700

    seperate event into its own vuex module

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events for {{ user.user.name }}</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link
         :to="{ name: 'event-list', query: { page: page - 1 } }"
@@ -10,7 +10,7 @@
       </router-link>
       |
     </template>
-    <template v-if="page * 3 < eventsTotal">
+    <template v-if="page * 3 < event.eventsTotal">
       <router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
         >Next Page</router-link
       >
@@ -37,7 +37,7 @@ export default {
       // What page we are currently on
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events', 'eventsTotal', 'user']),
+    ...mapState(['event', 'eventsTotal', 'user']),
   },
 }
 </script>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
     <div class="event-header">
-      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-      <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
-      <h5>Category: {{ event.category }}</h5>
+      <span class="eyebrow"
+        >@{{ event.event.time }} on {{ event.event.date }}</span
+      >
+      <h1 class="title">{{ event.event.title }}</h1>
+      <h5>
+        Organized by
+        {{ event.event.organizer ? event.event.organizer.name : '' }}
+      </h5>
+      <h5>Category: {{ event.event.category }}</h5>
     </div>
 
     <BaseIcon name="map"><h2>Location</h2></BaseIcon>
 
-    <address>{{ event.location }}</address>
+    <address>{{ event.event.location }}</address>
 
     <h2>Event details</h2>
-    <p>{{ event.description }}</p>
+    <p>{{ event.event.description }}</p>
 
     <h2>
       Attendees
       <span class="badge -fill-gradient">{{
-        event.attendees ? event.attendees.length : 0
+        event.event.attendees ? event.event.attendees.length : 0
       }}</span>
     </h2>
     <ul class="list-group">
       <li
-        v-for="(attendee, index) in event.attendees"
+        v-for="(attendee, index) in event.event.attendees"
         :key="index"
         class="list-item"
       >
