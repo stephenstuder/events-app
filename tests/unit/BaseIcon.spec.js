6954440b8b3d8107a65a25ac184dd0b3288c26ac
Stephen Studer <stephenkstuder@gmail.com>
2020-12-08 09:42:10 -0700

    unit testing added for base icon component

--- tests/unit/BaseIcon.spec.js
+++ tests/unit/BaseIcon.spec.js
@@ -2,8 +2,41 @@ import { shallowMount } from '@vue/test-utils'
 import baseIcon from '@/components/baseIcon.vue'
 
 describe('BaseIcon.vue', () => {
-  test('renders notification when prop is passed', () => {
+  test('If no props passed, default value set to 24 for height', async () => {
     const wrapper = shallowMount(baseIcon)
-    expect(true).toBe(true)
+    const height = parseInt(wrapper.find('svg').attributes('height'))
+    await wrapper.vm.$nextTick
+    expect(height).toBe(24)
+  })
+
+  test('If no props passed, default value set to 24 for width', async () => {
+    const wrapper = shallowMount(baseIcon)
+    const width = parseInt(wrapper.find('svg').attributes('width'))
+    await wrapper.vm.$nextTick
+    expect(width).toBe(24)
+  })
+
+  test('If height prop passed, height is set on the element', async () => {
+    const wrapper = shallowMount(baseIcon, {
+      propsData: {
+        height: 20,
+        width: 20
+      }
+    })
+    const height = parseInt(wrapper.find('svg').attributes('height'))
+    await wrapper.vm.$nextTick
+    expect(height).toBe(20)
+  })
+
+  test('If width prop is passed, width is set on the element', async () => {
+    const wrapper = shallowMount(baseIcon, {
+      propsData: {
+        height: 20,
+        width: 20
+      }
+    })
+    const width = parseInt(wrapper.find('svg').attributes('width'))
+    await wrapper.vm.$nextTick
+    expect(width).toBe(20)
   })
 })
